766202a06f9f4573b224f0bc799ce996cc15781c
Philip Noebauer <philip.noebauer@86400.com.au>
2025-03-21 12:18:04 +1100

    linked list insertion implemented

--- linked-list.js
+++ linked-list.js
@@ -4,9 +4,7 @@ class LinkedList {
     constructor(value) {
         this.head = {
             value: value,
-            next: {
-                next: null,
-            },
+            next: null,
         };
         this.tail = this.head;
         this.length = 1;
@@ -19,6 +17,50 @@ class LinkedList {
             value: prependValue,
             next: priorHeadNode,
         };
+        // const prependNode = {
+        //     value: prependValue,
+        //     next: null,
+        // };
+        // prependNode.next = this.head;
+        // this.head = prependNode;
+
+        this.length++;
+    }
+
+    insert(insertValue, position) {
+        if (position >= this.length - 1) {
+            const insertNode = {
+                value: insertValue,
+                next: null,
+            };
+            this.tail.next = insertNode;
+            this.tail = insertNode;
+            return this;
+        }
+
+        let i = 0;
+        let currentNode = this.head;
+        let nextNode = this.head.next;
+
+        for (let i = 0; i < position; i++) {
+            // console.dir({ currentNode, i }, { depth: null, colors: true });
+            currentNode = currentNode.next;
+
+            // currentNode = nextNode;
+            // nextNode = currentNode.next;
+        }
+
+        // // currentNode.next = {value: insertValue, next: }
+        // const insertNode = {value: insertValue, next: null}
+        // insertNode.next = currentNode.next
+
+        const insertNode = { value: insertValue, next: null };
+        insertNode.next = currentNode.next;
+
+        currentNode.next = insertNode;
+
+        // console.dir({ finalNode: currentNode }, { depth: null, colors: true });
+        // console.dir({ nextNode, currentNode }, { depth: null, colors: true });
 
         this.length++;
     }
@@ -26,13 +68,11 @@ class LinkedList {
     append(appendValue) {
         const appendNode = {
             value: appendValue,
-            next: {
-                next: null,
-            },
+            next: null,
         };
-        this.tail.next = appendNode
+        this.tail.next = appendNode;
         this.tail = appendNode;
-        
+
         // this.tail = appendNode;
         // let currentNode = this.head;
         // let next = this.head.next;
@@ -46,37 +86,45 @@ class LinkedList {
         // // Now currentNode is the last node
         // currentNode.next = appendNode; // Connect the last node to new tail
         // this.tail = appendNode;
-        // this.length++;
+        this.length++;
     }
 
     // calculate length
     getLength() {
-        let currentNode = this.head.next;
-        let next = this.head.next;
-        let length = 0;
-        while (next) {
-            next = currentNode.next;
-            currentNode = next;
+        let length = 1;
+
+        // let currentNode = this.head.next;
+        // let nextNode = this.head.next;
+        // while (nextNode) {
+        //     console.log('nextNode', nextNode)
+        //     nextNode = currentNode.next;
+        //     currentNode = nextNode;
+        //     length++;
+        // }
+        let nextNode = this.head.next;
+        while (nextNode) {
+            // console.log('nextNode', nextNode)
+            nextNode = nextNode.next;
             length++;
         }
 
-        return length
-
-        // let length = 0;
-        // let currentNode = this.head;
-        // let next = this.currentNode.next;
-        // let value = this.currentNode.value;
-        // while (next) {
-        //     currentNode = next;
-        //     next = currentNode.next;
-        //     length++;
-        // }
+        return length;
     }
 }
 
 const linkedList = new LinkedList(15);
 
 // linkedList.prepend(3);
+linkedList.append(5);
+// linkedList.append(6);
+
+// linkedList.insert(20, 1);
+
+linkedList.insert(16, 0);
+
+// linkedList.insert(18, linkedList.length);
+
+// linkedList.append(3);
 // linkedList.append(3);
 // console.dir(linkedList, { depth: null, colors: true });
 
@@ -84,7 +132,7 @@ const linkedList = new LinkedList(15);
 // linkedList.append(4);
 // linkedList.append(5);
 
-linkedList.prepend(12);
+// // linkedList.prepend(12);
 console.dir(linkedList, { depth: null, colors: true });
 
-console.log('linkedListgetLength', linkedList.getLength());
+// console.log('linkedListgetLength', linkedList.getLength());
